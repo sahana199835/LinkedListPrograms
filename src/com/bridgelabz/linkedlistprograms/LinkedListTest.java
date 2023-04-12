6f8ea77b7a4bd6a8de8a8c056aa915a02eb856b8
sahana199835 <tnsahana1@gmail.com>
2023-04-12 12:09:43 +0530

    add:.gitignore and README.md are files added

---
+++ src/com/bridgelabz/linkedlistprograms/LinkedListTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.linkedlistprograms;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+		linkedList.add(56);
+		linkedList.add(30);
+		linkedList.add(70);
+		System.out.println("Elements in the list: " + linkedList);
+	}
+}
+ 
+	
